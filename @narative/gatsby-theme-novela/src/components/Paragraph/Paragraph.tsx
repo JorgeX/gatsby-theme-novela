20eb2000f0641f92c3ce159b04a8d2b0b13bd540
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2019-08-23 01:18:30 -0700

    added theme-ui font sansSerif to Paragraph

--- @narative/gatsby-theme-novela/src/components/Paragraph/Paragraph.tsx
+++ @narative/gatsby-theme-novela/src/components/Paragraph/Paragraph.tsx
@@ -5,6 +5,7 @@ const Paragraph = styled.p`
   line-height: 1.756;
   font-size: 18px;
   color: ${p => p.theme.colors.articleText};
+  font-family: ${p => p.theme.fonts.sansSerif};
   transition: ${p => p.theme.colorModeTransition};
   margin: 0 auto 35px;
   width: 100%;
