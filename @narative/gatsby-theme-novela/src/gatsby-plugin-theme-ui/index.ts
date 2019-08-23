a521a13cb2acfec689b064c01cddbfb0d6f15143
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2019-08-23 01:22:55 -0700

    reverted back to original default fonts"

--- @narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui/index.ts
+++ @narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui/index.ts
@@ -15,7 +15,8 @@ const breakpoints = [
 
 const fonts = {
   serif: "'Merriweather', Georgia, Serif",
-  sansSerif: "'Ubuntu', 'Roboto', 'Noto', 'Segoe UI', 'Arial', sans-serif",
+  sansSerif:
+    "'SF Pro Display', '-apple-system', 'BlinkMacSystemFont', 'San Francisco', 'Helvetica Neue', 'Helvetica', 'Ubuntu', 'Roboto', 'Noto', 'Segoe UI', 'Arial', sans-serif",
   monospace: `"Operator Mono", Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace`
 };
 
