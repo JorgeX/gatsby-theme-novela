99ea8b526beef4423d287b34845b5b83f34c43f5
Sébastien Lorber <slorber@users.noreply.github.com>
2019-08-25 19:16:02 +0200

    Make unhandled social media url more explicit

--- @narative/gatsby-theme-novela/src/components/SocialLinks/SocialLinks.tsx
+++ @narative/gatsby-theme-novela/src/components/SocialLinks/SocialLinks.tsx
@@ -36,7 +36,9 @@ function SocialLinks({ links, fill = "#73737D" }: SocialLinksProps) {
       {links.map(option => {
         const name = getHostname(option.url);
         const Icon = icons[name];
-
+        if (!Icon) {
+          throw new Error(`unsupported social link name=${name} / url=${option.url}`);
+        }
         return (
           <SocialIconContainer
             key={option.url}
