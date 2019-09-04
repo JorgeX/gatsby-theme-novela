335cc99b72852dc661c50f21ba99eb9fc00925ad
brotzky <brotzky@gmail.com>
2019-09-03 17:20:47 -0700

    removing initial color mode

--- @narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui/index.ts
+++ @narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui/index.ts
@@ -24,8 +24,7 @@ const colorModeTransition =
   'background 0.25s var(--ease-in-out-quad), color 0.25s var(--ease-in-out-quad)';
 
 export default merge({
-  initialColorMode: 'light',
-  // useCustomProperties: true,
+  useCustomProperties: true,
   colorModeTransition,
   colors,
   fonts,
