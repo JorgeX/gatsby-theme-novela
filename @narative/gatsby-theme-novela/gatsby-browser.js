9860f3c8cbf76cc5f7f1f7f1bf75be398592646c
PBRT <pierre.beard@workangel.com>
2020-02-16 10:02:57 -0600

    fix: load fonts locally

--- @narative/gatsby-theme-novela/gatsby-browser.js
+++ @narative/gatsby-theme-novela/gatsby-browser.js
@@ -1,3 +1,6 @@
+// Load static fonts
+require('typeface-merriweather');
+
 exports.onInitialClientRender = require('./src/gatsby/browser/onInitialClientRender');
 exports.onRouteUpdate = require('./src/gatsby/browser/onRouteUpdate');
 exports.shouldUpdateScroll = require('./src/gatsby/browser/shouldUpdateScroll');
