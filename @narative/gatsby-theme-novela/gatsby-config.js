533d71d79128d43827b270e5bb4965b3bad7a3d3
Danny Steenman <danny.steenman@icloud.com>
2020-03-11 11:11:53 +0100

    Fix Contentful RSS feed url's

--- @narative/gatsby-theme-novela/gatsby-config.js
+++ @narative/gatsby-theme-novela/gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = ({
                       date: edge.node.date,
                       url: site.siteMetadata.siteUrl + edge.node.slug,
                       guid: site.siteMetadata.siteUrl + edge.node.slug,
-                      // custom_elements: [{ "content:encoded": edge.node.body }],
+                      custom_elements: [{ "content:encoded": edge.node.body }],
                       author: edge.node.author,
                     };
                   });
@@ -77,10 +77,10 @@ module.exports = ({
                       ...edge.node,
                       description: edge.node.excerpt,
                       date: edge.node.date,
-                      url: site.siteMetadata.siteUrl + edge.node.slug,
-                      guid: site.siteMetadata.siteUrl + edge.node.slug,
-                      // custom_elements: [{ "content:encoded": edge.node.body }],
-                      author: edge.node.author,
+                      url: site.siteMetadata.siteUrl + '/' + edge.node.slug,
+                      guid: site.siteMetadata.siteUrl + '/' + edge.node.slug,
+                      custom_elements: [{ "content:encoded": edge.node.body.childMarkdownRemark.html }],
+                      author: edge.node.author ? edge.node.author.name : '',
                     };
                   });
               } else {
@@ -95,7 +95,7 @@ module.exports = ({
                       url: site.siteMetadata.siteUrl + edge.node.slug,
                       guid: site.siteMetadata.siteUrl + edge.node.slug,
                       // custom_elements: [{ "content:encoded": edge.node.body }],
-                      author: edge.node.author,
+                      author: edge.node.author ? edge.node.author.name : '',
                     };
                   });
               }
@@ -107,6 +107,7 @@ module.exports = ({
                 allArticle(sort: {order: DESC, fields: date}) {
                   edges {
                     node {
+                      body
                       excerpt
                       date
                       slug
@@ -128,6 +129,11 @@ module.exports = ({
                       date
                       slug
                       title
+                      body {
+                        childMarkdownRemark {
+                          html
+                        }
+                      }
                       author {
                         name
                       }
@@ -142,6 +148,7 @@ module.exports = ({
                 allArticle(sort: {order: DESC, fields: date}) {
                   edges {
                     node {
+                      body
                       excerpt
                       date
                       slug
@@ -158,6 +165,11 @@ module.exports = ({
                       date
                       slug
                       title
+                      body {
+                        childMarkdownRemark {
+                          html
+                        }
+                      }
                       author {
                         name
                       }
@@ -199,30 +211,6 @@ module.exports = ({
               quality: 80,
               withWebp: true,
             },
-          },
-          {
-            resolve: `@raae/gatsby-remark-oembed`,
-            options: {
-              providers: {
-                include: ["Instagram"]
-              }
-            }
-          },
-          {
-            resolve: "gatsby-remark-embed-video",
-            options: {
-              width: 680,
-              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-              height: 400, // Optional: Overrides optional.ratio
-              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
-              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
-              urlOverrides: [
-                {
-                  id: 'youtube',
-                  embedURL: (videoId) => `https://www.youtube-nocookie.com/embed/${videoId}`,
-                }
-              ] //Optional: Override URL of a service provider, e.g to enable youtube-nocookie support
-            }
           },
           { resolve: `gatsby-remark-copy-linked-files` },
           { resolve: `gatsby-remark-numbered-footnotes` },
